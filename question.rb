cfedf5a9ca290fc0bcf8e50ce6ab46ebd836a849
Mauro Pereira Junior <mauroapjr@outlook.com>
2023-03-06 19:20:43 -0500

    fix bug after print second question and clean code

--- question.rb
+++ question.rb
@@ -1,20 +1,15 @@
-require 'active_support/all'
-
 class Question
-
   def initialize
     @number1 = rand(1..20)
     @number2 = rand(1..20)
     @sum = @number1 + @number2
   end
 
-  def get_question (name) #return the question
+  def get_question (name) 
     puts "#{name}: What does #{@number1} plus #{@number2} equal?"
   end
 
-  def check_answer (input)#check if the answer matches with the question
-    print "What is your answer?"
+  def check_answer (input)
     @sum == input
   end
-
 end
