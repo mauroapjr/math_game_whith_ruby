60419230df4bfcf319ab93b82f9c5ca96359b9d5
Mauro Pereira Junior <mauroapjr@outlook.com>
2023-03-06 19:18:50 -0500

    Add if statement to check the answer

--- players.rb
+++ players.rb
@@ -6,16 +6,12 @@ class Player
     @score = 3
   end
 
-  def lost_point #return the player score
+  def lost_point 
     @score -= 1
   end
 
   def game_over
-    loop do
-      if @score == 0
-        puts "Game Over"
-      end
-    end    
+    @score == 0
   end
 
   def new_question
@@ -23,10 +19,11 @@ class Player
     new_question.get_question(name)
     print '> '
     @playerchoice = $stdin.gets.chomp.to_i
-
+    if new_question.check_answer(@playerchoice.to_i)
+      puts 'Yes, you are ok'
+    else
+      puts "Nah..."
+      lost_point
+    end    
   end
-  
-  def update_score(number) #gonna do the math to update the scores 
-  end
-
 end
